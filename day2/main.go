33923b9404fd699de5f4c131b4b943cacedd1bc6
maximep <m.pinard@gmx.com>
2023-12-03 21:11:49 +0100

    Day 2 Part 2

--- day2/main.go
+++ day2/main.go
@@ -96,12 +96,14 @@ func partTwo() {
 
 	// Loop through 100 lines
 	for i := 0; i < len(result); i++ {
-		// Game init at sucess
-		isOkay := 1
 		// fmt.Println(result[i])
 		game := strings.Split(result[i], `: `) // Split game number from the rounds
 		rounds := strings.Split(game[1], `; `) // Split each round from the other rounds
 
+		// find max value for each game for each color
+		red := 0
+		green := 0
+		blue := 0
 		for _, round := range rounds {
 			colors := strings.Split(round, `, `) // Split each round from the other rounds
 			// val:= ""
@@ -109,27 +111,25 @@ func partTwo() {
 			for _, color := range colors {
 
 				values := strings.Split(color, " ")
-				fmt.Println(color)
+				// fmt.Println(color)
 				number, err := strconv.Atoi(values[0])
 				if err != nil {
 					fmt.Println("my bad !")
 				}
 
-				if values[1] == "red" && number > 12 {
-					isOkay = 0
+				if values[1] == "red" && number > red {
+					red = number
 				}
-				if values[1] == "green" && number > 13 {
-					isOkay = 0
+				if values[1] == "green" && number > green {
+					green = number
 				}
-				if values[1] == "blue" && number > 14 {
-					isOkay = 0
+				if values[1] == "blue" && number > blue {
+					blue = number
 				}
 			}
 
 		}
-		if isOkay == 1 {
-			total += i + 1 // Game 1 is index 0, so need to add 1 to index
-		}
+		total += (red * green * blue) // Game 1 is index 0, so need to add 1 to index
 	}
 	fmt.Printf("the total is: ")
 	fmt.Println(total)
