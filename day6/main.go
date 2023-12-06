d9c3504d66ebe9390c6be9a555b6b223b4ca383b
mpinard <maxime.pinard@ellona.io>
2023-12-06 14:04:35 +0100

    Day 6 Part 2

--- day6/main.go
+++ day6/main.go
@@ -7,6 +7,7 @@ package main
 
 import (
 	"fmt"
+	"math/big"
 	"os"
 	"regexp"
 	"strconv"
@@ -96,9 +97,73 @@ func partOne() {
 }
 
 func partTwo() {
+	fileContent := readFile()
+
+	// Split the lines
+	re := regexp.MustCompile(`[ ]{2,}`)
+	fileContent = strings.ReplaceAll(fileContent, "\r", "")
+	modifiedString := re.ReplaceAllString(fileContent, " ")
+
+	// Split the string by line breaks
+	rows := strings.Split(modifiedString, "\n")
+	distances := []*big.Int{}
+	times := []int{}
+	fmt.Printf("'%s'\n", rows[0])
+	fmt.Printf("'%s'\n", rows[1])
+	for index, row := range rows {
+		if index == 0 {
+			temp := strings.Split(row, ": ")[1]
+			str := strings.ReplaceAll(temp, " ", "")
+			num, err := strconv.Atoi(str)
+			if err != nil {
+				fmt.Printf("Error converting string '%s' to int: %s\n", str, err)
+				// Handle error (if needed)
+				continue // Skip to the next string if conversion fails
+			}
+			// Append the parsed int to the int array
+			times = append(times, num)
+		} else if index == 1 {
+			temp := strings.Split(row, ": ")[1]
+			str := strings.ReplaceAll(temp, " ", "")
+			num := new(big.Int)
+			num.SetString(str, 10)
+			// Append the parsed int to the int array
+			distances = append(distances, num)
+		}
+	}
+	fmt.Printf("times ")
+	fmt.Printf("'%d'\n", times)
+	fmt.Printf("distances ")
+	fmt.Printf("'%d'\n", distances)
+	total := 1
+	if len(distances) != len(times) {
+		fmt.Println("length do not match")
+	} else {
+		for i := 0; i < len(distances); i++ {
+			distanceToBeat := distances[i]
+			raceTime := times[i]
+			speed := 0
+			val := 0
+			for t := 1; t <= raceTime; t++ {
+				BigTime := new(big.Int)
+				BigTime.SetInt64(int64(raceTime - t))
+				speed++
+				result := new(big.Int).Mul(new(big.Int).SetInt64(int64(speed)), new(big.Int).SetInt64(int64(raceTime-t)))
+				if result.Cmp(distanceToBeat) > 0 {
+					val++
+				}
+			}
+			if val > 0 {
+				fmt.Printf("val for %d is : %d\n", i, val)
+				total *= val
+			}
+		}
+	}
+	fmt.Printf("total is : %d", total)
+	return
 }
 
 func main() {
-	partOne()
+	// partOne()
 	partTwo()
 }
