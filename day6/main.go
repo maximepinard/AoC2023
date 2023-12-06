78913bfcff1032cea3296b7f57fb9212b589f8c9
mpinard <maxime.pinard@ellona.io>
2023-12-06 13:40:08 +0100

    Day 6 Part 1

---
+++ day6/main.go
@@ -0,0 +1,104 @@
+/**
+Advent of Code 2023
+Maxime PINARD
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func readFile() string {
+	fileContent, err := os.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+	return string(fileContent)
+}
+
+func partOne() {
+	fileContent := readFile()
+
+	// Split the lines
+	re := regexp.MustCompile(`[ ]{2,}`)
+	fileContent = strings.ReplaceAll(fileContent, "\r", "")
+	modifiedString := re.ReplaceAllString(fileContent, " ")
+
+	// Split the string by line breaks
+	rows := strings.Split(modifiedString, "\n")
+	distances := []int{}
+	times := []int{}
+	fmt.Printf("'%s'\n", rows[0])
+	fmt.Printf("'%s'\n", rows[1])
+	for index, row := range rows {
+		if index == 0 {
+			temp := strings.Split(row, ": ")[1]
+			stringArray := strings.Split(temp, " ")
+			for _, str := range stringArray {
+				// Attempt to convert string to int
+				num, err := strconv.Atoi(str)
+				if err != nil {
+					fmt.Printf("Error converting string '%s' to int: %s\n", str, err)
+					// Handle error (if needed)
+					continue // Skip to the next string if conversion fails
+				}
+				// Append the parsed int to the int array
+				times = append(times, num)
+			}
+		} else if index == 1 {
+			temp := strings.Split(row, ": ")[1]
+			stringArray := strings.Split(temp, " ")
+			for _, str := range stringArray {
+				// Attempt to convert string to int
+				num, err := strconv.Atoi(str)
+				if err != nil {
+					fmt.Printf("Error converting string '%s' to int: %s\n", str, err)
+					// Handle error (if needed)
+					continue // Skip to the next string if conversion fails
+				}
+				// Append the parsed int to the int array
+				distances = append(distances, num)
+			}
+		}
+	}
+	fmt.Printf("times ")
+	fmt.Printf("'%d'\n", times)
+	fmt.Printf("distances ")
+	fmt.Printf("'%d'\n", distances)
+	total := 1
+	if len(distances) != len(times) {
+		fmt.Println("length do not match")
+	} else {
+		for i := 0; i < len(distances); i++ {
+			distanceToBeat := distances[i]
+			raceTime := times[i]
+			speed := 0
+			val := 0
+			for t := 0; t < raceTime; t++ {
+				if speed*(raceTime-t) > distanceToBeat {
+					val++
+				}
+				speed++
+			}
+			if val > 0 {
+				fmt.Printf("val for %d is : %d\n", i, val)
+				total *= val
+			}
+		}
+	}
+	fmt.Printf("total is : %d", total)
+	return
+}
+
+func partTwo() {
+}
+
+func main() {
+	partOne()
+	partTwo()
+}
