145233d7d116c519930881f2fda79e18be5793cc
maximep <m.pinard@gmx.com>
2023-12-09 22:26:17 +0100

    Day 1 & Day 2 Reworked

--- day1/main.go
+++ day1/main.go
@@ -1,197 +1,50 @@
 /**
 Advent of Code 2023
 Maxime PINARD
-using go
+Using go for the first time
 I am late to the party - made the 03/12/2023
+Remade the 09/12/2023, trying have a readable, optimized and short code
 */
 
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
-/**
-  PART 1
-*/
-
-func firstPart() {
-
-	fileContent, err := os.ReadFile("puzzle.json")
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-		return
+func AddFirstAndLastDigit(fileString string, part int) {
+	result := 0
+	for _, word := range strings.Fields(fileString) {
+		indexFirst := strings.IndexAny(word, "123456789")
+		indexLast := strings.LastIndexAny(word, "123456789")
+		first := string(word[indexFirst])
+		last := string(word[indexLast])
+		num, _ := strconv.Atoi(first + last)
+		result += num
 	}
-
-	var myArray []string
-
-	err = json.Unmarshal(fileContent, &myArray)
-	if err != nil {
-		fmt.Println("Error unmarshaling JSON:", err)
-		return
-	}
-
-	total := 0
-	for i := 0; i < len(myArray); i++ {
-		// fmt.Println(myArray[i])
-		s := myArray[i]
-		first := "0"
-		last := "0"
-		for j := 0; j < len(s); j++ {
-			if s[j] > 48 && s[j] < 58 {
-				if first == "0" {
-					first = string(s[j])
-					// fmt.Printf(first)
-				}
-				last = string(s[j])
-			}
-		}
-		// fmt.Println(last)
-		number, err := strconv.Atoi(first + last)
-		if err != nil {
-			fmt.Println("my bad !")
-		}
-		total = total + number
-	}
-	fmt.Println("Part 1")
-	fmt.Println(total)
+	fmt.Printf("Part %d is %d\n", part, result)
 }
 
-/**
-  PART 2
-*/
-
-func numberToString(number string) string {
-	val := "0"
-	switch number {
-	case "one":
-		val = "1"
-		break
-	case "two":
-		val = "2"
-		break
-	case "three":
-		val = "3"
-		break
-	case "four":
-		val = "4"
-		break
-	case "five":
-		val = "5"
-		break
-	case "six":
-		val = "6"
-		break
-	case "seven":
-		val = "7"
-		break
-	case "eight":
-		val = "8"
-		break
-	case "nine":
-		val = "9"
-		break
+func ReplaceDigitStringByInt(fileString string) {
+	stringToReplace := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	for index, s := range stringToReplace {
+		convert := strconv.Itoa(index + 1) // convert the index [1] of two => [1] + 1 to string
+		firstChar := string(s[0])          // seven = s7n so that sevenine = s7nine is to not break merge of two digit strings
+		lastChar := string(s[len(s)-1])
+		replace := firstChar + convert + lastChar
+		fileString = strings.ReplaceAll(fileString, s, replace)
 	}
-	return val
-}
-
-func readFile() []string {
-	fileContent, err := os.ReadFile("puzzle.json")
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-	}
-
-	var myArray []string
-
-	err = json.Unmarshal(fileContent, &myArray)
-	if err != nil {
-		fmt.Println("Error unmarshaling JSON:", err)
-	}
-	return myArray
-}
-
-func secondPart() {
-
-	total := 0
-
-	numbers := [9]string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
-
-	myArray := readFile()
-
-	for i := 0; i < len(myArray); i++ {
-		// fmt.Println(myArray[i])
-		row := myArray[i]
-
-		// each row should have a first and last value
-		first := "0"
-		last := "0"
-
-		// each character of row is looped through to search for a number
-		for j := 0; j < len(row); j++ {
-
-			// check the byte value of the char to see if it is a digit !
-			if row[j] > 48 && row[j] < 58 {
-				if first == "0" {
-					first = string(row[j])
-				}
-				last = string(row[j])
-			}
-
-			// loop for each possible written digit: [one, two, ..., nine]
-			for g := 0; g < len(numbers); g++ {
-				// if the current index of the char of the row is big enough to search for that written digit, if so then do it
-				if len(numbers[g])-1 <= j {
-					test := myArray[i][j-(len(numbers[g])-1) : j+1] // [0:3] to get 012 because start is inclusive but end is exclusive
-					val := numberToString(test)
-					// if the value is found then go places
-					if val != "0" {
-						// if first was not overwritten then save that value in it
-						if first == "0" {
-							first = val
-						}
-						// replace last anyway
-						last = val
-					}
-				}
-			}
-		}
-
-		// fmt.Println(first + last)
-		number, err := strconv.Atoi(first + last)
-		if err != nil {
-			fmt.Println("my bad !")
-		}
-		total = total + number
-	}
-	fmt.Println("Part 2")
-	fmt.Println(total)
+	AddFirstAndLastDigit(fileString, 2)
 }
 
 func main() {
-	firstPart()
-	secondPart()
+	fileContent, _ := os.ReadFile("input.txt")
+	fileString := string(fileContent)
+	// Part 1
+	AddFirstAndLastDigit(fileString, 1)
+	// Part 2
+	ReplaceDigitStringByInt(fileString)
 }
-
-/**
-Advent of Code 2023
-
-First day coding in GO
-
-Things i have learned in GO
-
-loop through each char of a string give the byte value of the char
-bye value of digits are from 49 (0) to 57 (9)
-
-string() to convert byte to string
-strconv.Atoi() to convert string to int
-...
-os.readFile to read a file
-json.Unmarshal() to readJson into something that match
-no need for parenthesis in if and for statements
-
-myString[0:3] is getting the first substring corresponding the first 3 char but the start 0 is inclusive for the end 3 is exclusive !
-
-*/
--- day2/main.go
+++ day2/main.go
@@ -3,6 +3,7 @@ Advent of Code 2023
 Maxime PINARD
 using go
 I am late to the party - made the 03/12/2023
+Remade the 09/12/2023, trying have a readable, optimized code
 */
 
 package main
@@ -14,115 +15,28 @@ import (
 	"strings"
 )
 
-func readFile() string {
-	fileContent, err := os.ReadFile("puzzle.txt")
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-	}
-	return string(fileContent)
-}
-
-func partOne() {
-	fileContent := readFile()
-	total := 0
-	// colorsStrings := [3]string{"red", "green", "blue"}
-
-	// Split the lines
-	result := strings.Split(fileContent, `
-`)
-
-	// Loop through 100 lines
-	for i := 0; i < len(result); i++ {
-		// Game init at sucess
-		isOkay := 1
-		// fmt.Println(result[i])
-		game := strings.Split(result[i], `: `) // Split game number from the rounds
-		rounds := strings.Split(game[1], `; `) // Split each round from the other rounds
-
-		for _, round := range rounds {
-			colors := strings.Split(round, `, `) // Split each round from the other rounds
-			// val:= ""
-
-			for _, color := range colors {
-
-				values := strings.Split(color, " ")
-				//fmt.Println(color)
-				number, err := strconv.Atoi(values[0])
-				if err != nil {
-					fmt.Println("my bad !")
-				}
-
-				if values[1] == "red" && number > 12 {
-					isOkay = 0
-				}
-				if values[1] == "green" && number > 13 {
-					isOkay = 0
-				}
-				if values[1] == "blue" && number > 14 {
-					isOkay = 0
-				}
-			}
-
-		}
-		if isOkay == 1 {
-			total += i + 1 // Game 1 is index 0, so need to add 1 to index
-		}
-	}
-	fmt.Printf("the total is: ")
-	fmt.Println(total)
-}
-
-func partTwo() {
-	fileContent := readFile()
-	total := 0
-	// colorsStrings := [3]string{"red", "green", "blue"}
-
-	// Split the lines
-	result := strings.Split(fileContent, `
-`)
-
-	// Loop through 100 lines
-	for i := 0; i < len(result); i++ {
-		// fmt.Println(result[i])
-		game := strings.Split(result[i], `: `) // Split game number from the rounds
-		rounds := strings.Split(game[1], `; `) // Split each round from the other rounds
-
-		// find max value for each game for each color
-		red := 0
-		green := 0
-		blue := 0
-		for _, round := range rounds {
-			colors := strings.Split(round, `, `) // Split each round from the other rounds
-			// val:= ""
-
-			for _, color := range colors {
-
-				values := strings.Split(color, " ")
-				// fmt.Println(color)
-				number, err := strconv.Atoi(values[0])
-				if err != nil {
-					fmt.Println("my bad !")
-				}
-
-				if values[1] == "red" && number > red {
-					red = number
-				}
-				if values[1] == "green" && number > green {
-					green = number
-				}
-				if values[1] == "blue" && number > blue {
-					blue = number
-				}
-			}
-
-		}
-		total += (red * green * blue) // Game 1 is index 0, so need to add 1 to index
-	}
-	fmt.Printf("the total is: ")
-	fmt.Println(total)
-}
-
 func main() {
-	partOne()
-	partTwo()
+	fileContent, _ := os.ReadFile("input.txt")
+	fileString := string(fileContent)
+
+	part1, part2 := 0, 0
+	for i, row := range strings.Split(fileString, "\n") {
+		game := strings.Split(row, `: `)       // Split game number from the rounds
+		rounds := strings.Split(game[1], `; `) // Split each round from the other rounds
+		num := map[string]int{}                // map each game color to a number
+		for _, round := range rounds {
+			colors := strings.Split(round, `, `) // Split each round from the other rounds
+			for _, color := range colors {
+				values := strings.Split(color, " ")
+				number, _ := strconv.Atoi(values[0])
+				num[values[1]] = max(num[values[1]], number) // get the max of each color for the game
+			}
+		}
+		if num["red"] <= 12 && num["green"] <= 13 && num["blue"] <= 14 {
+			part1 += i + 1
+		}
+		part2 += num["red"] * num["green"] * num["blue"]
+	}
+	fmt.Printf("Part 1 is %d\n", part1)
+	fmt.Printf("Part 2 is %d\n", part2)
 }
